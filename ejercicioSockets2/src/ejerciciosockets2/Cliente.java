db09516615cfceb6a573531946ee267bf0827850
Sanchez70 <kevinalexsanchez2051@gmail.com>
2024-04-22 18:37:52 -0500

    Cambio1.1

--- ejercicioSockets2/src/ejerciciosockets2/Cliente.java
+++ ejercicioSockets2/src/ejerciciosockets2/Cliente.java
@@ -16,51 +16,50 @@ import java.net.Socket;
  * @author jeans
  */
 public class Cliente {
-    public static void main(String[] args){
-        String ipPrincipal = " 192.168.100.6",ipSecundario="";
-        int puertoPrincipal = 8080 , puertoSecundario=0;
+
+    public static void main(String[] args) {
+        String ipPrincipal = " 192.168.100.6", ipSecundario = "", puertoVal = "";
+        int puertoPrincipal = 8080, puertoSecundario = 0;
         DataInputStream in;
         DataOutputStream out;
         BufferedReader teclado = null;
-        boolean c = true;
-        while (c) {
-            try {
-                while(ipSecundario.equals("") || puertoSecundario  0){
-                    
-                }
+        boolean c = true, validador = true;
+
+        try {
+            while (validador == true) {
                 System.out.println("INGLRE LA IP ");
                 teclado = new BufferedReader(new InputStreamReader(System.in));
                 ipSecundario = teclado.readLine();
-                if (ipSecundario.equals("")) {
-                                        
-                }
-                
                 System.out.println("INGLRE EL PUERTO ");
                 teclado = new BufferedReader(new InputStreamReader(System.in));
                 puertoSecundario = Integer.parseInt(teclado.readLine());
-                Socket sc = new Socket(HOST, PUERTO);
-                c=false;
-                System.out.println("Ingrese la ");
-                while (true) {
-                    in = new DataInputStream(sc.getInputStream());
-                    out = new DataOutputStream(sc.getOutputStream());
-                    teclado = new BufferedReader(new InputStreamReader(System.in));
-                    String mensaje1 = teclado.readLine();
-                    if (mensaje1.matches("^[a-z]+[A-Z]+S$")) {
-                        System.out.println("entre");
-                        out.writeUTF(mensaje1);
-                        String mensaje = in.readUTF();
-                        System.out.println(mensaje);                   
-                   
-                    } else {
-                        System.out.println("Incorrecto");
-                    }
-
+                puertoVal = String.valueOf(puertoSecundario);
+                if (ipSecundario.equals("192.168.100.6") && puertoVal.equals("8080")) {
+                    validador = false;
+                } else {
+                    System.out.println("Error de conexion. Ingrese nuevamente!!!!");
                 }
-
-            } catch (Exception e) {
-                System.out.println("ERROR AL CONECTARSE");
             }
+            Socket sc = new Socket(ipSecundario, puertoSecundario);
+            System.out.println("Ingrese la ");
+            while (true) {
+                in = new DataInputStream(sc.getInputStream());
+                out = new DataOutputStream(sc.getOutputStream());
+
+                String mensaje1 = teclado.readLine();
+                if (mensaje1.matches("^[a-z]+$")) {
+                    System.out.println("entre");
+                    out.writeUTF(mensaje1);
+                    String mensaje = in.readUTF();
+                    System.out.println(mensaje);
+                } else {
+                    System.out.println("Incorrecto");
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("ERROR AL CONECTARSE");
         }
     }
+
 }
